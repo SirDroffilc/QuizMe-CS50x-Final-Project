740453233542d88d04c6ad9451c0cc0a9f1c60e6
Ford Torion <clifford.torion04@gmail.com>
2024-10-16 11:08:28 +0800

    started heroku deploy, stopped at postgresql

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from web_app.app import create_app
 
-flask_app = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
     
     
